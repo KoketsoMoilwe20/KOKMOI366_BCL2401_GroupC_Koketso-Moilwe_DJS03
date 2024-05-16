688afe4ca658c599435e55c152ba4683f5698c4b
Koketso Moilwe <koketsomoilwe2@gmail.com>
2024-05-16 13:13:56 +0200

    move code inside function

--- scripts.js
+++ scripts.js
@@ -6,29 +6,31 @@ let matches = books;
 const getDOM = (selector) => document.querySelector(selector);//getting DOM elements
 
 const bookPreviews = (books, container) => {
+    const starting = document.createDocumentFragment();
+
+    for (const { author, id, image, title } of matches.slice(0, BOOKS_PER_PAGE)) {
+        const element = document.createElement('button')
+        element.classList = 'preview'
+        element.setAttribute('data-preview', id);
     
-}
+        element.innerHTML = `
+            <img
+                class="preview__image"
+                src="${image}"
+            />
+            
+            <div class="preview__info">
+                <h3 class="preview__title">${title}</h3>
+                <div class="preview__author">${authors[author]}</div>
+            </div>
+        `
+    
+        starting.appendChild(element)
+} //function to create and append book previews
 
-const starting = document.createDocumentFragment()
 
-for (const { author, id, image, title } of matches.slice(0, BOOKS_PER_PAGE)) {
-    const element = document.createElement('button')
-    element.classList = 'preview'
-    element.setAttribute('data-preview', id)
 
-    element.innerHTML = `
-        <img
-            class="preview__image"
-            src="${image}"
-        />
-        
-        <div class="preview__info">
-            <h3 class="preview__title">${title}</h3>
-            <div class="preview__author">${authors[author]}</div>
-        </div>
-    `
 
-    starting.appendChild(element)
 }
 
 document.querySelector('[data-list-items]').appendChild(starting)
